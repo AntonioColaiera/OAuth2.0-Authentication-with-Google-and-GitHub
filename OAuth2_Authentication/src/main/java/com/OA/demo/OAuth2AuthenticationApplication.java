813b377a1e52275d195c68ab84cf3195c1fbe886
Antonio Colaierà <colaiera@gmail.com>
2023-06-20 10:01:37 +0200

    Add files via upload

---
+++ OAuth2_Authentication/src/main/java/com/OA/demo/OAuth2AuthenticationApplication.java
@@ -0,0 +1,13 @@
+ package com.OA.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AuthenticationApplication.class, args);
+	}
+
+}
---
+++ OAuth2_Authentication/src/main/java/com/OA/demo/config/package-info.java
@@ -0,0 +1,1 @@
+package com.OA.demo.config;
